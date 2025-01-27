f4c0c651dfe536293d2887593586f70faf80cd55
Betinho1990 <robertofilho.apoena@gmai.com>
2025-01-27 17:47:33 -0300

    validação de numero total da lista serem pares

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,28 +1,25 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
+let amigosSorteados = [];
 let input = document.getElementById('amigo');
 let lista = document.getElementById('listaAmigos');
 
     function adicionarAmigo() {
         console.log('adicionar nome')
-        let campoDigit = input.value.trim();
         
-
-
+        let campoDigit = input.value.trim();
+                
         if (campoDigit ==='') {
             console.log('campo vazio')
             alert('Por favor insira um nome!');
             return;    
         }
-
-            if (amigos.includes(campoDigit)) {
+        
+        if (amigos.includes(campoDigit)) {
                 
                 alert('Esse nome já esta participando!');
                 input.value = "";
                 return;
-            }
-            
-                
+        }
                 amigos.push(campoDigit);
             
         let novoNome = document.createElement('li');
@@ -31,40 +28,46 @@ let lista = document.getElementById('listaAmigos');
             
             atualizarLista();
 
-            input.value = '';
+            input.value = '';   
 }
     function atualizarLista(){
         console.log('atualizarlista')
                 lista.innerHTML = '';
             }
-            for (let i = 0; i < amigos.length; i++) {
-                let novoNome = document.createElement('li');
+        for (let i = 0; i < amigos.length; i++) {
+             let novoNome = document.createElement('li');
                 novoNome.textContent = amigos[i];
                 lista.appendChild(novoNome);
-            }
-
-
-    function sortearAmigo() {
+}
+function sortearAmigo() {
         
-        if (amigos.length === 0) {
+    if (amigos.length === 0) {
         return alert('Não há amigos na lista');
+    }   
+    if (amigos.length % 2 !== 0) {
+        alert("A lista de participantes terminou com um número ímpar de nomes. Por favor, adicione mais um nome para que a lista fique com um número par de participantes.");
+        return;
     }
         let indiceAleatorio = Math.floor(Math.random() * amigos.length);
-        
+    
         let amigoSelecionado = amigos[indiceAleatorio];
-        console.log(`O amigo selecionado é: ${amigoSelecionado}`);
-        
-        let resultadoElement = document.getElementById("resultado");
-        
-        if (resultadoElement) {
-            resultadoElement.textContent = `O amigo sorteado é: ${amigoSelecionado}`;
-         } else {
-            console.error('Elemento com o ID "resultado" não encontrado.');
-     }
-
-        return amigoSelecionado;
+            console.log(`O amigo selecionado é: ${amigoSelecionado}`);
+    if (amigosSorteados.includes(amigoSelecionado)) {
+                // Se já foi sorteado, sorteia novamente
+    return sortearAmigo();
     }
- 
+        amigosSorteados.push(amigoSelecionado);
+
+        let resultadoElement = document.getElementById("resultado");
+    
+    if (resultadoElement) {
+        resultadoElement.textContent = `O amigo sorteado é: ${amigoSelecionado}`;
+    } else {
+        console.error('Elemento com o ID "resultado" não encontrado.');
+    }
+
+} 
+        
                 document.querySelector('.button-add').addEventListener('click', function() {
                 let campoDigit = input.value.trim();
             if (campoDigit !== '') {
